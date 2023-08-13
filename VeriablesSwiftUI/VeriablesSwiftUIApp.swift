fbbe9365a503337863252c593f31632f7063ae38
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-13 15:19:39 +0300

    Initial Commit

---
+++ VeriablesSwiftUI/VeriablesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeriablesSwiftUIApp.swift
+//  VeriablesSwiftUI
+//
+//  Created by İbrahim Halid Bayrak on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct VeriablesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
